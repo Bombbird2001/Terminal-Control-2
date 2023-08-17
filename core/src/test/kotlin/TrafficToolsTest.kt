9a4e3991658709306de0fa3cd416d896ff686f92
Shang Yi Ku <kushangyi2001@gmail.com>
2023-08-17 22:41:29 +0800

    Increased time between departures when there is a negative backlog of departures

--- core/src/test/kotlin/TrafficToolsTest.kt
+++ core/src/test/kotlin/TrafficToolsTest.kt
@@ -9,16 +9,16 @@ object TrafficToolsTest: FunSpec() {
             calculateAdditionalTimeToNextDeparture(24, 40) shouldBe 0
             calculateAdditionalTimeToNextDeparture(15, 40) shouldBe 0
             calculateAdditionalTimeToNextDeparture(10, 40) shouldBe 0
-            calculateAdditionalTimeToNextDeparture(3, 40) shouldBe 28
-            calculateAdditionalTimeToNextDeparture(-14, 40) shouldBe 96
-            calculateAdditionalTimeToNextDeparture(-6, 20) shouldBe 96
-            calculateAdditionalTimeToNextDeparture(-20, 40) shouldBe 120
-            calculateAdditionalTimeToNextDeparture(-29, 40) shouldBe 210
-            calculateAdditionalTimeToNextDeparture(-58, 80) shouldBe 210
-            calculateAdditionalTimeToNextDeparture(-40, 40) shouldBe 320
-            calculateAdditionalTimeToNextDeparture(-47, 40) shouldBe 320
-            calculateAdditionalTimeToNextDeparture(-30, 20) shouldBe 320
-            calculateAdditionalTimeToNextDeparture(-15, 10) shouldBe 320
+            calculateAdditionalTimeToNextDeparture(3, 40) shouldBe 56
+            calculateAdditionalTimeToNextDeparture(-14, 40) shouldBe 192
+            calculateAdditionalTimeToNextDeparture(-6, 20) shouldBe 192
+            calculateAdditionalTimeToNextDeparture(-20, 40) shouldBe 240
+            calculateAdditionalTimeToNextDeparture(-29, 40) shouldBe 420
+            calculateAdditionalTimeToNextDeparture(-58, 80) shouldBe 420
+            calculateAdditionalTimeToNextDeparture(-40, 40) shouldBe 640
+            calculateAdditionalTimeToNextDeparture(-47, 40) shouldBe 640
+            calculateAdditionalTimeToNextDeparture(-30, 20) shouldBe 640
+            calculateAdditionalTimeToNextDeparture(-15, 10) shouldBe 640
         }
     }
 }
