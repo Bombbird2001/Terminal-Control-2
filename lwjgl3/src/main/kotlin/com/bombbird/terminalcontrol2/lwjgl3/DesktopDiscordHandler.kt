72542dba10a901321bd91b8ed8735ac2e640c418
Shang Yi Ku <kushangyi2001@gmail.com>
2024-01-27 16:23:58 +0800

    Load appropriate native library file depending on OS and architecture

--- lwjgl3/src/main/kotlin/com/bombbird/terminalcontrol2/lwjgl3/DesktopDiscordHandler.kt
+++ lwjgl3/src/main/kotlin/com/bombbird/terminalcontrol2/lwjgl3/DesktopDiscordHandler.kt
@@ -7,6 +7,7 @@ import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
 import ktx.assets.toInternalFile
+import java.io.File
 import java.time.Instant
 
 class DesktopDiscordHandler: DiscordHandler {
@@ -18,10 +19,47 @@ class DesktopDiscordHandler: DiscordHandler {
     private lateinit var gameActivity: Activity
     private var gameTimeSet = false
 
+    private fun getNativeLibrary(): File? {
+        val name = "discord_game_sdk"
+        var osName = System.getProperty("os.name").lowercase()
+        var arch = System.getProperty("os.arch").lowercase()
+
+        if (arch == "x86_64") arch = "amd64"
+
+        val objectName: String
+
+        if (osName.contains("windows")) {
+            osName = "windows_$arch"
+            objectName = "$name.dll"
+        } else if (osName.contains("linux")) {
+            osName = "linux"
+            objectName = "$name.so"
+        } else if (osName.contains("mac os")) {
+            osName = "macos"
+            objectName = "$name.dylib"
+        } else {
+            FileLog.warn("DesktopDiscordHandler", "Unknown OS: $osName")
+            return null
+        }
+
+        return "libs/$osName/$objectName".toInternalFile().file()
+    }
+
     override fun initialize() {
         discordThread = Thread {
-            val discordLibrary = "Libs/windows_x86_64/discord_game_sdk.dll".toInternalFile()
-            Core.init(discordLibrary.file())
+            val nativeLibrary = getNativeLibrary()
+            if (nativeLibrary == null) {
+                FileLog.warn("DesktopDiscordHandler", "Failed to initialize Discord Game SDK: Native library not found")
+                return@Thread
+            }
+
+            try {
+                Core.init(nativeLibrary)
+                FileLog.info("DesktopDiscordHandler", "Discord Game SDK initialized")
+            } catch (e: UnsatisfiedLinkError) {
+                FileLog.warn("DesktopDiscordHandler", "Failed to initialize Discord Game SDK:\n$e")
+                return@Thread
+            }
 
             val params = CreateParams().apply {
                 clientID = Secrets.DISCORD_GAME_SDK_APP_ID
@@ -44,7 +82,6 @@ class DesktopDiscordHandler: DiscordHandler {
         }
         running = true
         discordThread.start()
-        FileLog.info("DesktopDiscordHandler", "Discord Game SDK initialized")
     }
 
     override fun updateInGame(mapIcao: String, planesInControl: Int, playersInGame: Int, maxPlayers: Int,
